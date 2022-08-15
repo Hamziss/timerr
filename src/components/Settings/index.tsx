a551323e6c033106b61b4c23a46a05bb36289d2b
Hamza <hamzachebbah9999@gmail.com>
2022-08-15 22:18:37 +0100

    added switcher

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -2,14 +2,13 @@ import VolumeDown from "@mui/icons-material/VolumeDown"
 import VolumeUp from "@mui/icons-material/VolumeUp"
 import FormControlLabel from "@mui/material/FormControlLabel"
 import Slider from "@mui/material/Slider"
-import { styled } from "@mui/material/styles"
-import Switch, { SwitchProps } from "@mui/material/Switch"
 import TextField from "@mui/material/TextField"
 import React, { useEffect, useState } from "react"
 import { NOTE } from "../../../utils/constants"
 import useClickOutside from "../../hooks/useClickOutside"
 import useStore from "../../zustand/store"
 import classes from "./style.module.css"
+import { IOSSwitch } from "./switcher"
 
 type Props = {
 	setShowSettings: (showSettings: boolean) => void
@@ -214,54 +213,5 @@ const Settings = ({ setShowSettings, settingsZustand }: Props) => {
 		</>
 	)
 }
-const IOSSwitch = styled((props: SwitchProps) => (
-	<Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
-))(({ theme }) => ({
-	width: 42,
-	height: 26,
-	padding: 0,
-	"& .MuiSwitch-switchBase": {
-		padding: 0,
-		margin: 2,
-		transitionDuration: "300ms",
-		"&.Mui-checked": {
-			transform: "translateX(16px)",
-			color: "#fff",
-			"& + .MuiSwitch-track": {
-				backgroundColor: theme.palette.mode === "dark" ? "#2ECA45" : "#65C466",
-				opacity: 1,
-				border: 0,
-			},
-			"&.Mui-disabled + .MuiSwitch-track": {
-				opacity: 0.5,
-			},
-		},
-		"&.Mui-focusVisible .MuiSwitch-thumb": {
-			color: "#33cf4d",
-			border: "6px solid #fff",
-		},
-		"&.Mui-disabled .MuiSwitch-thumb": {
-			color:
-				theme.palette.mode === "light"
-					? theme.palette.grey[100]
-					: theme.palette.grey[600],
-		},
-		"&.Mui-disabled + .MuiSwitch-track": {
-			opacity: theme.palette.mode === "light" ? 0.7 : 0.3,
-		},
-	},
-	"& .MuiSwitch-thumb": {
-		boxSizing: "border-box",
-		width: 22,
-		height: 22,
-	},
-	"& .MuiSwitch-track": {
-		borderRadius: 26 / 2,
-		backgroundColor: theme.palette.mode === "light" ? "#E9E9EA" : "#39393D",
-		opacity: 1,
-		transition: theme.transitions.create(["background-color"], {
-			duration: 500,
-		}),
-	},
-}))
+
 export default Settings
