68dfb63e6b854708bc649ca5f85e65cfd74a273c
Hamza <hamzachebbah9999@gmail.com>
2022-09-06 16:02:59 +0100

    squaled event handlers

---
+++ src/components/Timer/handlers.ts
@@ -0,0 +1,88 @@
+import { toast } from "react-toastify"
+import { updateFavicon } from "../../../utils/helpers"
+import { ISettings } from "../../types/settings"
+
+export default function completeHandler({
+	settings,
+	setmode,
+	setTimer,
+	pomodoroCount,
+	mode,
+	setPomodoroCount,
+	start,
+	reset,
+	updateUserSession,
+	status,
+}: any) {
+	if (settings.activeAlarm) {
+		const audio = new Audio()
+		audio.src = "/sounds/alarm.mp3"
+		audio.loop = false
+		audio.volume = settings.alarmVolume / 100
+		audio.play()
+	}
+	setPomodoroCount(pomodoroCount + 1)
+	if (mode === "POMODORO" && pomodoroCount < settings.longbreakevery) {
+		setmode("SHORT_BREAK")
+		setTimer(settings.shortbreak)
+
+		if (status === "authenticated") {
+			updateUserSession(settings.timepomodoro)
+			toast.success(
+				`Congrats! You Won ${settings.timepomodoro * 50} Coins & Tree!`
+			)
+		}
+		reset()
+		if (settings.autoStartShortBreaks) {
+			start()
+		}
+	} else if (mode === "POMODORO" && pomodoroCount === settings.longbreakevery) {
+		setmode("LONG_BREAK")
+		setTimer(settings.longbreak)
+		if (status === "authenticated") {
+			updateUserSession(settings.timepomodoro)
+			toast.success(
+				`Congrats! You Won ${settings.timepomodoro * 50} Coins & Tree!`
+			)
+		}
+		reset()
+		if (settings.autoStartLongBreaks) {
+			start()
+		}
+	} else if (mode === "SHORT_BREAK") {
+		setmode("POMODORO")
+		setTimer(settings.timepomodoro)
+		reset()
+		if (settings.autoStartPomodoros) {
+			start()
+		}
+	} else if (mode === "LONG_BREAK") {
+		setmode("POMODORO")
+		setTimer(settings.timepomodoro)
+		reset()
+		if (settings.autoStartPomodoros) {
+			start()
+		}
+	}
+}
+
+export const handleMode = (
+	mode: string,
+	setmode: Function,
+	setTimer: Function,
+	settings: ISettings,
+	reset: Function
+) => {
+	if (mode === "SHORT_BREAK") {
+		setmode("SHORT_BREAK")
+		setTimer(settings.shortbreak)
+	} else if (mode === "LONG_BREAK") {
+		setmode("LONG_BREAK")
+		setTimer(settings.longbreak)
+	} else if (mode === "POMODORO") {
+		setmode("POMODORO")
+		setTimer(settings.timepomodoro)
+	}
+	reset()
+	updateFavicon(mode)
+}
---
+++ src/types/settings.d.ts
@@ -0,0 +1,11 @@
+export interface ISettings {
+	timepomodoro: number
+	shortbreak: number
+	longbreak: number
+	longbreakevery: number
+	autoStartPomodoros: boolean
+	autoStartShortBreaks: boolean
+	autoStartLongBreaks: boolean
+	activeAlarm: boolean
+	alarmVolume: number
+}
