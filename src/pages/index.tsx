6c1177b3d7eee71700dc2d3a40e5d5513d2a9f52
Hamza <hamzachebbah9999@gmail.com>
2022-09-07 09:50:36 +0100

    image optimization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,7 +42,7 @@ const Home: NextPage<Props> = ({ quote }) => (
 		</section>
 		<section className={classes.motivationSection}>
 			<div className={classes.cirleBoyContainer}>
-				<Image layout="fill" src={cirleBoy} />
+				<Image priority placeholder="blur" src={cirleBoy} />
 			</div>
 			<div className={classes.quoteContainer}>
 				<div className={classes.sparkle}>
@@ -56,7 +56,7 @@ const Home: NextPage<Props> = ({ quote }) => (
 				</div>
 			</div>
 			<div className={classes.cirleGirlContainer}>
-				<Image layout="fill" placeholder="blur" src={cirleGirl} />
+				<Image priority placeholder="blur" src={cirleGirl} />
 			</div>
 		</section>
 
