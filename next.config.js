9fee712b0c3bd2ecfd0863d9e9dcbebbd43aa0de
Hamza <hamzachebbah9999@gmail.com>
2022-09-21 04:47:33 +0100

    delete unused package

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-        domains: ["res.cloudinary.com"],
-    },
-}
-
+        reactStrictMode: true,
+        swcMinify: true,
+        images: {
+            domains: ["res.cloudinary.com"],
+        },
+    }
+    // const withBundleAnalyzer = require("@next/bundle-analyzer")({
+    //     enabled: process.env.ANALYZE === "true",
+    // })
 module.exports = nextConfig
+    // module.exports = withBundleAnalyzer(nextConfig)
